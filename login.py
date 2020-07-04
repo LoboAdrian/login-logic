a59750da755367cc4088fa278933095c24d6d2a8
Adrian Lobo <44212477+LoboAdrian@users.noreply.github.com>
2020-07-03 22:25:24 -0400

    Update login.py

--- login.py
+++ login.py
@@ -1,8 +1,10 @@
 import time
 
+# New or returning user?
 log = input(str("Create account?(y/n): "))
 
 
+# Register your username and password, if a new user
 def signup():
     write = open("signup.txt", "w")
     newuser = input(str("Username: "))
@@ -15,23 +17,29 @@ def signup():
     time.sleep(3)
 
 
+# Check if your login info is correct, if a returning user
 def logs():
     username = input(str("Username: "))
     password = input(str("Password: "))
     read = open("signup.txt", "r")
+    # Turn the .txt file into a list
     r = read.readlines()
     newInfo = []
     for newData in r:
         newInfo.append(newData.strip())
+    # If login info is correct
     if username == newInfo[0] and password == newInfo[1]:
         print("Logged In")
         time.sleep(3)
+    # If login info is wrong
     else:
         print("Login failed")
         logs()
 
 
+# New or returning user
 if log == "y":
     signup()
 else:
     logs()
+
